cf4fa4cdcb6c9e3fb29955368574bd024d999f70
Riccel Lazatin <rmlazatin@student.hau.edu.ph>
2026-03-19 18:43:34 +0800

    Fixed shop design

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,13 +7,8 @@ function Footer() {
     <div className="footer">
         <div className="footer-container">
             <div className="footer-one">
-                <h1 className="footer-email">baconteam@gmail.com</h1>
+                <p className="footer-email">baconteam@gmail.com</p>
                 <p className="footer-number">+63 992 123 4567</p>
-                <p className="footer-sub">Have some Bacon</p>
-            </div>
-
-            <div className="footer-two">
-                <p className="footer-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
         </div>
     </div>
