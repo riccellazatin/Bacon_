cd7091d4cc2f2477b0a44117b7888fec5d5a1557
Riccel Lazatin <rmlazatin@student.hau.edu.ph>
2026-03-23 15:02:47 +0800

    frontend fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:8000/api';
+const API_BASE = process.env.REACT_APP_API_BASE || 'https://bacon-backend-nu68.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
